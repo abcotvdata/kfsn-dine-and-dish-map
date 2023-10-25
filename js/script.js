61685a217f859b939d403f9f0b19ff0ba8707bfc
grace-manthey <45363240+grace-manthey@users.noreply.github.com>
2023-10-25 09:42:36 -0700

    update map tiles

--- js/script.js
+++ js/script.js
@@ -50,8 +50,8 @@ $(document).ready(function(){ // begin document.ready block
 
 
 	    //tile layer
-		L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
-			attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 			subdomains: 'abcd',
 			opacity: 0.25,
 	    	pane: 'overlayPane',
